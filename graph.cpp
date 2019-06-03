dce0e1e39674a7d712efd581976195124cfc324c
Mahdi Sabbouri <48333875+msabbouri@users.noreply.github.com>
2019-06-03 10:45:23 -0700

    Source file

---
+++ graph.cpp
@@ -0,0 +1,75 @@
+#include "graph.hpp"
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void graph :: add_edge(int a, int b){
+
+	if(a == b)
+		return;
+	else if(has_edge(a, b))
+		return;
+
+	//Occurs if the top two conditions are not met
+	adjList.at(a).push_back(b);
+	++edges;
+	
+}
+
+bool graph :: has_edge(int a, int b){
+
+	for(auto n : adjList.at(a)){
+		if(n == b)
+			return true;
+		}
+
+	return false;
+	
+}
+
+int graph :: count_nodes(){
+	return nodes;
+}
+
+int graph :: count_edges(){
+	return edges;	
+}
+
+vector<int> graph :: bfs(int n){
+
+	vector<int> nodeDistance(nodes, INT_MAX);
+
+	nodeDistance.at(n) = 0;
+
+	queue<int> queue;
+	queue.push(n);
+
+
+	while (!queue.empty()) {
+		int n = queue.front();
+		queue.pop();
+
+		for (auto i : adjList.at(n)) {
+
+			if (nodeDistance.at(i) == INT_MAX) { 
+
+				nodeDistance.at(i) = nodeDistance.at(n) + 1;
+				queue.push(i);
+			}
+		}
+	}
+	return nodeDistance;
+}
+
+bool graph :: is_connected(int a, int b){
+
+	vector<int> breadthSearch = bfs(a);
+
+	if(breadthSearch.at(b) == INT_MAX)
+		return false;
+
+	else
+		return true;
+	
+}
